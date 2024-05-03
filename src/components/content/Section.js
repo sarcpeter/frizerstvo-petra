bb6597476c51bc92cdb893b5e68da1d1237de99b
Peter Šarc <sarc.peter@gmail.com>
2024-05-03 09:29:30 +0200

    feat(Section): Added background image support (#1).

--- src/components/content/Section.js
+++ src/components/content/Section.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledSection = styled.section`
+import BackgroundImage from 'gatsby-image';
 
+const StyledSection = styled.section`
+  position: relative;
+  min-height: 50px;
+  
+  &:has(div img) {
+    background: rgba(0, 0, 0, 0.6);
+  }
 `;
+
+const StyledBackgroundImage = styled(BackgroundImage)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
+`;
+
 
 const Section = ({
   id,
   className,
   children,
-  style
+  style,
+  background
 }) => {
+
   return (
     <StyledSection id={id} className={className} style={style}>
+      {
+        background &&
+        <StyledBackgroundImage
+          fluid={background.childImageSharp.fluid}
+          style={{position: 'absolute'}}
+        />
+      }
       {children}
     </StyledSection>
   );
