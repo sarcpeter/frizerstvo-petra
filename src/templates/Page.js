9f21c59e9058aa79ca79122a9a0c30941665d498
Peter Šarc <sarc.peter@gmail.com>
2024-08-08 13:22:45 +0200

    feat: Added Page template (#20)

---
+++ src/templates/Page.js
@@ -0,0 +1,57 @@
+import React, {useEffect} from 'react';
+import {apiPlugin, storyblokInit, StoryblokComponent} from 'gatsby-source-storyblok';
+
+import Layout from '../components/layout/Layout';
+
+const Page = ({ pageContext }) => {
+  const blok = pageContext.node;
+
+  function loadStoryblokBridge() {
+    const {StoryblokBridge, location} = window;
+    const storyblokInstance = new StoryblokBridge();
+    storyblokInstance.on(['publish', 'chnage'], () => location.reload());
+  }
+
+  function loadBridgeScript() {
+    const existingScript = document.getElementById('storyblokBridge');
+
+    if (!existingScript) {
+      const script = document.createElement('script');
+      script.src = 'https://app.storyblok.com/f/storyblok-v2-latest.js';
+      script.id = 'storyblokBridge';
+
+      document.body.appendChild(script);
+
+      script.onload = () => { loadStoryblokBridge(); };
+    } else {
+      loadStoryblokBridge();
+    }
+  }
+
+  storyblokInit({
+    accessToken: process.env.GATSBY_PREVIEW_STORYBLOK,
+    use: [apiPlugin],
+    components: {
+
+    }
+  });
+
+  useEffect(() => {
+    if (window.location.search.includes('_storyblok')) {
+      loadBridgeScript();
+    }
+    // ! Intentionally ignoring react-hooks exhaustive-deps warning !
+    // loadBridgeScript is stable and should only be called upon first render.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Layout title={blok.name}>
+      {blok.content.body.map((nestedBlok) => (
+        <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+      ))}
+    </Layout>
+  );
+}
+
+export default Page;
